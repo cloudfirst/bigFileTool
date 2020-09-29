9cff4b84c12ba69ea8f59a6614dbe73a8f773eb2
luhya <luhya@macosx-MacBook.local>
2020-09-29 19:45:21 +0800

    add install files for mac os x .

---
+++ ext/installer/mac_os_x/postins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
+
+OSX_RELEASE=`sw_vers -productVersion | cut -d . -f 1,2`
+
+cd "/Library/Application Support/OxFold/BigFileTool"
+
+chown -R $USER:staff myrouter
+chown -R $USER:staff client
+
+exit 0
