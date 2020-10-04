68654d2d69ba0a0913432993831ba01ed716b1e0
Your Name <you@example.com>
2020-10-04 11:01:05 +0800

    update linux install script.

--- ext/installer/linux/install.sh
+++ ext/installer/linux/install.sh
@@ -3,3 +3,5 @@ sudo mkdir -p /usr/local/bigfiletool/client
 sudo chown -R $USER:$USER  /usr/local/bigfiletool
 
 sudo cp ./bigfiletool_dist/*  /usr/local/bigfiletool/
+
+sudo apt-get install libxcb-xinerama0 libpng12-0
